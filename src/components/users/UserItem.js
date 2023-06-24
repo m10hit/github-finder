2222b31d1b9a8f3acd43f459b1c06f434a926b36
Mohit Chauhan <mohitchauhan10@gmail.com>
2023-06-24 18:17:07 +0530

    add more files

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt="avatar"
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
